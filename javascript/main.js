e8f4c6aa612ab7f2efc0673e46387f2b47663590
MarijaTV <marijatv@gmail.com>
2021-06-12 13:04:00 +0300

    After lesson

--- javascript/main.js
+++ javascript/main.js
@@ -7,7 +7,8 @@ Taip pat parašykite funkciją kuri: isikviesdama save pačią atspausdina skai�
 
 */
 firstCountItems (data.length);
-secondCountKaunasItems (); //nesuprantu, kodėl neveikia
+secondCountKaunasItems (data); //nesuprantu, kodėl neveikia
+// countKaunasmeniu (data);
 
 function firstCountItems(data) {
     console.log(data); //duomenų atvaizdavimas
@@ -20,21 +21,17 @@ function firstCountItems(data) {
     }
 }
 // Filtruojame duomenis
-function secondFilterKaunasItems(data) {
-    let countKaunas = arrayOfObject.filter(function(obj){
-        //loop through each object
-        for(key in obj){
-          //check if object value contains value you are looking for
-          if(obj[key].includes('Kaunas')){
-            //add this object to the filtered array
-            return obj;
-            }
-           }
-          });
-            
-      console.log(countKaunas);
+function secondCountKaunasItems(data) {
+    let count = 0;
+    data.forEach(kavine =>{
+        if (kavine.adresas.toLowerCase().includes('kaunas')) {
+            count++;
+        }
+    })
+    console.log(count);
 }
-    
+
+// countKaunasmeniu (data)
 
 
 
@@ -45,3 +42,50 @@ function rekursija(counter) {
     }
     console.log(counter);
 };
+
+
+
+var userChoice = prompt("Do you choose rock, paper or scissors?");
+var computerChoice = Math.random();
+if (computerChoice < 0.34) {
+    computerChoice = "rock";
+} else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+} else {
+    computerChoice = "scissors";
+}
+
+var compare = function(choice1, choice2) {
+    if(choice1 === choice2) {
+    return "The result is a tie!";
+}
+if(choice1 === "rock") {
+    if(choice2 === "scissors") {
+        return "rock wins";
+    } else {
+        return "paper wins";
+    }
+}
+if(choice1 === "paper") {
+    if(choice2 === "rock") {
+        return "paper wins";
+    } else {
+        if(choice2 === "scissors") {
+            return "scissors wins";
+    }
+}
+if(choice1 === "scissors") {
+    if(choice2 === "rock") {
+        return "rock wins";
+    } else {
+        if(choice2 === "paper") {
+            return "scissors wins";
+        }
+    }
+}
+}
+};
+console.log("User Choice: " + userChoice);
+console.log("Computer Choice: " + computerChoice);
+compare(userChoice, computerChoice);
+
